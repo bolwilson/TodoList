7eef22eccbde95cbfc4059a1d17d8994e6044846
Wilson Bol <wilson.majok@gmail.com>
2017-08-23 03:08:13 +0200

    Updated

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -17,6 +17,10 @@ $("input[type='text']").on('keypress', function(e) {
 		$(this).val('');
 
 		// Create a new li and add to ul
-		$('ul').append('<li><span>X</span> ' + todoText + '</li>')
+		$('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + '</li>')
 	}
 });
+
+$('.fa-pencil').on('click', function() {
+	$("input[type='text']").fadeToggle();
+})
